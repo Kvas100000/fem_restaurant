15d7ad489e092f26a6c78410c3813e780bd16edc
Kvas100000 <sasa0500432532@gmail.com>
2025-11-11 18:48:18 +0200

    финальное добавление

--- myproject/restik/models.py
+++ myproject/restik/models.py
@@ -1,11 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 class Customer(models.Model):
-    name = models.CharField(max_length=20)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=20, default='Anonim')
     phone = models.CharField(max_length=20)
+
     def __str__(self):
-        return self.user.username
+        return self.user.username if self.user else self.name
+
 
 class Waiter(models.Model):
     name = models.CharField(max_length=100)
@@ -27,7 +30,7 @@ class MenuCategory(models.Model):
         return self.name
 
 class MenuItem(models.Model):
-    category =models.ForeignKey(MenuCategory,on_delete=models.CASCADE)
+    category =models.ForeignKey(MenuCategory,on_delete=models.CASCADE, related_name='items')
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -39,11 +42,14 @@ class Reservation(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
     waiter = models.ForeignKey(Waiter, on_delete=models.SET_NULL, null=True, blank=True)
-    reservation_datetime = models.DateTimeField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
     notes = models.TextField(blank=True)
 
     def __str__(self):
-        return f"{self.customer.user.username} - {self.reservation_datetime}"
+        return f"{self.customer} — {self.start_time}"
+
+
 
 
 class OrderedItem(models.Model):
@@ -53,7 +59,6 @@ class OrderedItem(models.Model):
 
     def __str__(self):
         return f"{self.menu_item.name} x{self.quantity}"
-
 
 
 
--- myproject/restik/urls.py
+++ myproject/restik/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import index, place_list, menu_category
+from unicodedata import category
+
+from .views import index, place_list, menu_category, category_detail, order_detail, book_table, order_list
 
 urlpatterns = [
     path("", index, name ="index"),
     path("place-list", place_list, name ="place-list"),
     path("menu-category", menu_category, name ="menu-category"),
+    path('menu/<int:category_id>/', category_detail, name='category_detail'),
+    path('book-table/', book_table, name='book_table'),
+    path('orders/', order_list, name = 'order_list'),
+    path('orders/<int:reservation_id>/', order_detail, name='order_detail'),
 ]
+
+
